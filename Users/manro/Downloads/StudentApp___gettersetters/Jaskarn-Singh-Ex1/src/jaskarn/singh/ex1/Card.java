a9ed953ecd5bd80d7465d2ad657c598cbcea1366
Jaskarn-Singh-1234 <sin12580.sheridancollege.ca>
2023-02-02 16:50:04 -0500

    WEEK One

---
+++ Users/manro/Downloads/StudentApp___gettersetters/Jaskarn-Singh-Ex1/src/jaskarn/singh/ex1/Card.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package jaskarn.singh.ex1;
+
+/**
+ *
+ * @author $ Jaskarn Singh
+ */
+public class Card {
+
+      private String suit; //clubs, spades, diamonds, hearts
+   private int value;//1-13
+
+   public static final String [] SUITS = {"Hearts", "Diamonds", "Spades", "Clubs"};
+    /**
+     * @return the suit
+     */
+    public String getSuit() {
+        return suit;
+    }
+
+    /**
+     * @param suit the suit to set
+     */
+    public void setSuit(String suit) {
+        this.suit = suit;
+    }
+
+    /**
+     * @return the value
+     */
+    public int getValue() {
+        return value;
+    }
+
+    /**
+     * @param value the value to set
+     */
+    public void setValue(int value) {
+        this.value = value;
+}
+}
